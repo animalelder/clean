b981d7e180a204d2761fc3eafc479592a2e48ed3
Aaron Dantley <aadantley@gmail.com>
2025-01-31 13:22:31 -0500

    fix (terms): removing back button import

--- app/(unauthorized)/Terms/page.js
+++ app/(unauthorized)/Terms/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import BackButton from "@/components/Foundation/BackButton";
 import TermsSheet from "@/components/Terms/TermsSheet";
 
 export default function Terms() {
