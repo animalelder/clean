1cebf24d34d581e8b928b8f30160feb05e729a79
Aaron Dantley <aadantley@gmail.com>
2025-02-04 09:43:02 -0500

    fix (hero section):
    updating text color for mobile using tailwind css

--- components/Landing/hero-section.js
+++ components/Landing/hero-section.js
@@ -11,11 +11,11 @@ export function HeroSection() {
     <section className="relative min-h-[100vh] flex flex-col justify-end text-center text-white">
       <VideoBackground videoSources={videoSources} />
       <div className="container relative z-10 px-4 mx-auto mb-8">
-        <p className="max-w-3xl mx-auto mb-6 text-lg italic sm:text-xl md:text-2xl lg:text-3xl text-white/90">
+        <p className="max-w-3xl mx-auto mb-6 text-lg italic sm:text-xl md:text-2xl lg:text-3xl text-white/90 max-md:text-black">
           Discover your purpose and grow into the man God designed you to be.
         </p>
         <button className="inline-flex flex-col items-center gap-2 px-6 py-3 transition-transform rounded-lg group hover:-translate-y-1 ">
-          <span className="text-base font-medium sm:text-lg">
+          <span className="text-base font-medium sm:text-lg max-md:text-black">
             Take the Step
           </span>
           <span className="animate-bounce">↓</span>
