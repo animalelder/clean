a7fc21bd94bf0f1f8d423b9ce4d452acf50d5b81
Aaron Dantley <aadantley@gmail.com>
2025-02-03 21:50:39 -0500

    fix (hero section):
    - added the clean video with the testimonial captures to hero section

--- components/Landing/hero-section.js
+++ components/Landing/hero-section.js
@@ -6,17 +6,18 @@ import VideoBackground from "./LocalMultipleVideoPlayer";
 export function HeroSection() {
   const videoSources = [
     "/videos/Hero Video Corrected.mov",
-    // Add more video sources as needed
+    "/videos/Hero Video Corrected.mov",
+    "/videos/Hero Video Corrected.mov",
   ];
 
   return (
-    <section className="relative min-h-[calc(100vh-4rem)] flex items-center justify-center text-center text-white">
+    <section className="relative min-h-[100vh] flex flex-col justify-end text-center text-white">
       <VideoBackground videoSources={videoSources} />
-      <div className="container relative px-4 py-20 mx-auto">
-        <h1 className="mb-6 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+      <div className="container relative px-4 mx-auto mb-8">
+        {/* <h1 className="mb-6 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
           Build True Brotherhood.
-        </h1>
-        <p className="mb-12 text-xl italic sm:text-2xl md:text-3xl text-white/90">
+        </h1> */}
+        <p className="mb-6 text-xl italic sm:text-2xl md:text-3xl text-white/90">
           Discover your purpose and grow into the man God designed you to be.
         </p>
         <button className="inline-flex flex-col items-center gap-2 transition-transform group hover:-translate-y-1">
