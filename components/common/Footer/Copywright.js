f7e2e1366002c4f530be93476da562e36daf3709
Aaron Dantley <aadantley@gmail.com>
2025-02-12 08:29:23 -0500

    feat (footer):
    updating footer with info on 501c3 status

--- components/common/Footer/Copywright.js
+++ components/common/Footer/Copywright.js
@@ -3,6 +3,14 @@ export default function Copywright() {
     <div className="text-sm text-center text-gray-400 sm:text-left">
       Thirty Mighty Men Ministries &trade;. &copy; {new Date().getFullYear()};
       all rights reserved.
+      <p></p>
+      Thirty Mighty Men Ministries Inc. is a Section 501(c)(3) charitable
+      organization, EIN 87-2757799.
+      <p></p>
+      All donations are deemed tax-deductible absent any limitations on
+      deductibility applicable to a particular tax payer.
+      <p></p>
+      no goods or services were provided in exchange for your contribution.
     </div>
   );
 }
