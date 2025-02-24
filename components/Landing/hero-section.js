a80631482fa789ff783d8737d2332746132fd8a6
animalelder <wllearned@icloud.com>
2025-02-24 14:53:21 -0800

    Changes to the prettier config. The class sorting plugin was not inlined correctly. HeroSection was a test file using the new config. All the Tailwind classes were sorted properly.

--- components/Landing/hero-section.js
+++ components/Landing/hero-section.js
@@ -11,34 +11,32 @@ export function HeroSection() {
   const videoSources = ["/videos/hero-3_1080_16-9.mp4"];
 
   return (
-    <section className="relative after:absolute after:inset-0 after:bg-black/15 mt-[85px] flex flex-col justify-end w-full min-h-[100dvh] text-center">
+    <section className="relative mt-[85px] flex min-h-[100dvh] w-full flex-col justify-end text-center after:absolute after:inset-0 after:bg-black/15">
       <VideoBackground videoSources={videoSources} />
       <div className="container relative z-10 px-4 mx-auto mb-24 md:text-white">
-        <p className="max-w-3xl mx-auto mb-8 text-lg italic text-black md:text-white drop-shadow-xl sm:text-xl md:text-2xl lg:text-3xl">
+        <p className="max-w-3xl mx-auto mb-8 text-lg italic text-black drop-shadow-xl sm:text-xl md:text-2xl md:text-white lg:text-3xl">
           Discover your purpose and grow into the man God designed you to be.
         </p>
         <div className="space-x-3">
           <Link href="/individuals">
-            <Button className="text-white drop-shadow-md bg-primary-red hover:bg-red-500">
+            <Button className="text-white bg-primary-red drop-shadow-md hover:bg-red-500">
               For Individuals
             </Button>
           </Link>
           <Link href="/churches">
-            <Button className="text-white drop-shadow-md bg-accent-red hover:bg-red-500">
+            <Button className="text-white bg-accent-red drop-shadow-md hover:bg-red-500">
               For Churches
             </Button>
           </Link>
           <button
-            className="inline-flex flex-row items-center gap-2 px-2 py-2 text-white transition-transform rounded-lg drop-shadow-md bg-almost-black hover:bg-gray-800 group hover:-translate-y-1"
+            className="inline-flex flex-row items-center gap-2 px-2 py-2 text-white transition-transform rounded-lg group bg-almost-black drop-shadow-md hover:-translate-y-1 hover:bg-gray-800"
             onClick={() => {
               document
                 .getElementById("TwoFaces")
                 .scrollIntoView({ behavior: "smooth" });
             }}
           >
-            <span className="text-base font-medium text-white ">
-              Learn More
-            </span>
+            <span className="text-base font-medium text-white">Learn More</span>
             <span className="text-white animate-bounce">↓</span>
           </button>
         </div>
