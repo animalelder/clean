99f17d4b83252f82086a264a42e63b05c3e026c4
Aaron Dantley <aadantley@gmail.com>
2025-02-26 15:40:35 -0500

    fix (scholarship application):
    updating to change folder name hopefully

--- app/Scholarship-Application/page.js
+++ app/Scholarship-Application/page.js
@@ -4,7 +4,7 @@ export default function ScholarshipApplication() {
   return (
     <div className="container px-4 py-8 mx-auto">
       <h1 className="mb-8 text-4xl font-bold text-center">
-        CLEAN Scholarship Application
+        CLEAN Scholarship Application 
       </h1>
       <ScholarshipForm />
     </div>
