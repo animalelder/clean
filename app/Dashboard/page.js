a165e798b1520bac73616b75422d0e99b27f25f3
Aaron Dantley <aadantley@gmail.com>
2025-03-01 00:02:35 -0500

    feat (sidebar):
    - adding a shadcn sidebar to dashboard

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,19 +1,22 @@
 "use client";
 
 import React from "react";
-import { SignedIn } from "@clerk/nextjs";
-import DailySurvey from "@/components/Dashboard/DailySurvey";
+import { DailySurveySidebar } from "@/components/Dashboard/daily-survey-sidebar";
 import MainBody from "@/components/Dashboard/MainBody";
 import NavigationBar from "@/components/Dashboard/NavigationBar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Dashboard() {
   return (
-    <SignedIn>
-      <div className="relative mx-16 flex min-h-screen flex-col items-center justify-start">
+    <div className="relative mx-16 flex min-h-screen flex-col items-center justify-start">
+      <SidebarProvider defaultOpen={false}>
+        <SidebarTrigger />
+        <div className="w-64 shrink-0">
+          <DailySurveySidebar />
+        </div>
         <NavigationBar />
-        <DailySurvey />
         <MainBody />
-      </div>
-    </SignedIn>
+      </SidebarProvider>
+    </div>
   );
 }
