8db2c32a457202af2ff44e558bbb2ef7f5d931d1
Aaron Dantley <aadantley@gmail.com>
2025-03-01 08:29:51 -0500

    fix (sidebar):
    - removing sidebar implementation; moving this work to another feature branch

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,22 +1,14 @@
 "use client";
 
 import React from "react";
-import { DailySurveySidebar } from "@/components/Dashboard/daily-survey-sidebar";
 import MainBody from "@/components/Dashboard/MainBody";
 import NavigationBar from "@/components/Dashboard/NavigationBar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Dashboard() {
   return (
     <div className="relative mx-16 flex min-h-screen flex-col items-center justify-start">
-      <SidebarProvider defaultOpen={false}>
-        <SidebarTrigger />
-        <div className="w-64 shrink-0">
-          <DailySurveySidebar />
-        </div>
-        <NavigationBar />
-        <MainBody />
-      </SidebarProvider>
+      <NavigationBar />
+      <MainBody />
     </div>
   );
 }
