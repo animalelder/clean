90936bdb31d717b44c00934d8309e5efdd951919
animalelder <wllearned@icloud.com>
2025-05-18 10:08:26 -0700

    feat: EmailOTPClient plugin for auth-client and adjusted some options for OneTapClient

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,16 +1,22 @@
-import { adminClient, oneTapClient } from "better-auth/client/plugins";
+import {
+  adminClient,
+  emailOTPClient,
+  oneTapClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
 import { toast } from "sonner";
 
 export const authClient = createAuthClient({
   plugins: [
     adminClient(),
+    emailOTPClient(),
     oneTapClient({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
       cancelOnTapOutside: false,
       autoSelect: false,
       promptOptions: {
-        maxAttempts: 1,
+        baseDelay: 2000,
+        maxAttempts: 5,
       },
     }),
   ],
