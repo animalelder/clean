c072198ae47935787deeca005e6964c6b5f056ed
animalelder <wllearned@icloud.com>
2025-05-18 09:51:25 -0700

    feat: add Google OneTap to SignUp route page

--- app/SignUp/page.js
+++ app/SignUp/page.js
@@ -1,11 +1,45 @@
 "use client";
 
+import { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import SignUp from "@/components/auth/sign-up";
-import { signIn } from "@/lib/auth-client";
+import { authClient, signIn } from "@/lib/auth-client";
 
 export default function SignIn() {
+  const router = useRouter();
+  const CALLBACK_URL = "/profile";
+
+  useEffect(() => {
+    authClient.oneTap({
+      fetchOptions: {
+        onError: ({ error }) => {
+          toast.error(error.message || "An error occurred");
+        },
+        onSuccess: () => {
+          toast.success("Successfully signed in");
+          router.push(CALLBACK_URL);
+        },
+      },
+    });
+    // Some Notes on useEffect and the Dependency Array:
+    //
+    // ESLint think the router goes in the dependency array, but it is not
+    // necessary because the effect is not dependent on the state of the router.
+    // Weirdly, ESLint doesn't want toast to be added to the dependency array.
+    //
+    // If router is a dependency, it creates a weird non-fatal dev error
+    // that does not seem to happen when the dependency array is empty.
+    // The error is related to FedCM, which is the new API for federated login
+    // used in OneTap. I have not tested the page on a browser with Intelligent
+    // Tracking Prevention (ITP) enabled, but the Sign In With Google button
+    // is there as a fallback. The UX is nicer with OneTap.
+    //
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="inset-auto flex min-h-screen w-screen min-w-[400px] flex-col md:flex-row">
       <div className="fixed left-2 top-2 z-20 rounded-md bg-white/80 py-1 before:static max-md:shadow-md" />
@@ -32,13 +66,15 @@ export default function SignIn() {
             await signIn.social(
               {
                 provider: "google",
-                callbackURL: "/profile",
+                callbackURL: CALLBACK_URL,
               },
               {
                 onRequest: (ctx) => {
+                  console.dir(ctx);
                   setLoading(true);
                 },
                 onResponse: (ctx) => {
+                  console.dir(ctx);
                   setLoading(false);
                 },
               },
